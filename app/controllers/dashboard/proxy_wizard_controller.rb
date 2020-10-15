89c101199ecbcd993e202193c9b1c447d67c78c0
Steven <steven.wanderski@gmail.com>
2020-10-15 13:21:27 -0500

    Adds a basic proxy wizard

---
+++ app/controllers/dashboard/proxy_wizard_controller.rb
@@ -0,0 +1,76 @@
+class Dashboard::ProxyWizardController < AuthenticatedController
+  def step_one
+    @proxy = Proxy.new
+  end
+
+  def step_two
+    @proxy = current_user.proxies.find(params[:id])
+  end
+
+  def step_three
+    @proxy = current_user.proxies.find(params[:id])
+  end
+
+  def step_four
+    @proxy = current_user.proxies.find(params[:id])
+  end
+
+  def create
+    @proxy = Proxy.new(proxy_params)
+    @proxy.user = current_user
+
+    if @proxy.save
+      redirect_to dashboard_proxy_wizard_step_two_path(@proxy)
+    else
+      flash[:error] = 'Proxy could not be saved. Check below.'
+      render :step_name
+    end
+  end
+
+  def update
+    @proxy = current_user.proxies.find(params[:id])
+
+    if @proxy.update(proxy_params)
+
+      path = case params[:step]
+        when '2'
+          dashboard_proxy_wizard_step_three_path
+        when '3'
+          dashboard_proxy_wizard_step_four_path
+        when '4'
+          edit_dashboard_proxy_path(@proxy)
+        end
+
+      redirect_to path
+    else
+      flash[:error] = 'Proxy could not be saved. Check below.'
+      render :edit
+    end
+  end
+
+  private
+
+  def proxy_params
+    params.require(:proxy).permit(
+      :name,
+      :proxy_url,
+      :proxy_request_type,
+      :target_url,
+      :target_request_type,
+      :body_format,
+      :slug,
+      proxy_params_attributes: [
+        :id,
+        :param_from,
+        :param_to,
+        :_destroy
+      ],
+      proxy_headers_attributes: [
+        :id,
+        :name,
+        :value,
+        :_destroy
+      ]
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,14 @@ Rails.application.routes.draw do
 
   namespace :dashboard do
     resources :proxies
+
+    get '/proxy_wizard/new/step_one', to: 'proxy_wizard#step_one', as: :proxy_wizard_step_one
+    get '/proxy_wizard/:id/step_two', to: 'proxy_wizard#step_two', as: :proxy_wizard_step_two
+    get '/proxy_wizard/:id/step_three', to: 'proxy_wizard#step_three', as: :proxy_wizard_step_three
+    get '/proxy_wizard/:id/step_four', to: 'proxy_wizard#step_four', as: :proxy_wizard_step_four
+    post '/proxy_wizard', to: 'proxy_wizard#create'
+    put '/proxy_wizard/:id', to: 'proxy_wizard#update', as: :proxy_wizard_update
+
     get '/settings', to: 'settings#index'
     put '/settings', to: 'settings#update'
   end
