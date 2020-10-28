c69eb4da322b624a9e0a7abfd7abcba1efa2c790
Steven <steven.wanderski@gmail.com>
2020-10-28 10:50:55 -0500

    Sets headers only if matching origin exists

--- app/controllers/proxies_controller.rb
+++ app/controllers/proxies_controller.rb
@@ -53,9 +53,11 @@ class ProxiesController < ApplicationController
       allow_origin = origin
     end
 
-    headers['Access-Control-Allow-Origin'] = allow_origin
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    if allow_origin
+      headers['Access-Control-Allow-Origin'] = allow_origin
+      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    end
   end
 end
